37b1fada2cbd9c10027d4e33030bdc1f5b6a00e4
jrobertson627 <jessicarobertson627@gmail.com>
2026-05-26 10:44:47 -0700

    Text change for Porkbun test

--- personal-site-app/src/App.tsx
+++ personal-site-app/src/App.tsx
@@ -14,7 +14,7 @@ function App() {
 
   return (
     <main className="container">
-      <h1>Welcome to Tauri + React</h1>
+      <h1>Welcome to Jessica Robertson's site</h1>
 
       <div className="row">
         <a href="https://vite.dev" target="_blank">
